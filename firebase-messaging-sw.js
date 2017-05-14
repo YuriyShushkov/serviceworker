093d8f5b9ec6a94027033ed7899276f4ae539c69
Yura <Yura@SHUSHKOV>
2017-05-14 20:19:53 +0300

    version 005

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,7 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+//importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
+//importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+
+
 
 firebase.initializeApp({
     'messagingSenderId': '448358493027'
