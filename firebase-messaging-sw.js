05acc9e0f934e3301b8b481ff42df59281ec540a
Yura <Yura@SHUSHKOV>
2017-05-15 20:47:18 +0300

    V 021

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,7 @@
 //importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 //importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 
 
