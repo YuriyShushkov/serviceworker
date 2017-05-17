8c1eb8aa31ee3763925a61b7c37ef73845732d5b
Шушков Юрий <shushkov@gmail.com>
2017-05-17 06:02:35 +0300

    ver 027

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,28 @@
 //importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 //importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-auth.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
+var config = {
+    apiKey: "AIzaSyBnpUoRO0jeYspTiFaNklR5hpsWgCmS-_Q",
+    authDomain: "nzproject-ee70d.firebaseapp.com",
+    databaseURL: "https://nzproject-ee70d.firebaseio.com",
+    projectId: "nzproject-ee70d",
+    storageBucket: "nzproject-ee70d.appspot.com",
+    messagingSenderId: "932318795709"
+};
 
 
-firebase.initializeApp({
-    'messagingSenderId': '932318795709'
-});
+firebase.initializeApp(config);
+// Retrieve Firebase Messaging object.
+const messaging = firebase.messaging(
 
-firebase.messaging();
+//firebase.initializeApp({
+//    'messagingSenderId': '932318795709'
+//});
+
+//firebase.messaging();
 
 self.addEventListener('notificationclick', function(event) {
     const target = event.notification.data.click_action || '/';
